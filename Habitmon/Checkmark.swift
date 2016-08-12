0afb18a6ac0756c6be055e075b539b65e93a512c
Valerie <val.conklin@gmail.com>
2016-08-12 16:21:23 -0700

    Cleans up checkmark-related code

--- Habitmon/Checkmark.swift
+++ Habitmon/Checkmark.swift
@@ -16,7 +16,6 @@ class Checkmark: UIView {
     super.init(coder: aDecoder)
     
     let checkbox = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-    //checkbox.backgroundColor = UIColor.redColor()
     
     let emptyCheckboxImage = UIImage(named: "emptyCheckbox")
     let checkedOffImage = UIImage(named: "checkedOff")
@@ -33,11 +32,7 @@ class Checkmark: UIView {
   
   func checkboxTapped(checkbox: UIButton) {
     print("Ka-ching!")
-    //checkbox.backgroundColor = UIColor.greenColor()
-    print(checkbox.selected)
     checkbox.selected = true
-    print(checkbox.selected)
-//    checkbox.setImage(<#T##image: UIImage?##UIImage?#>, forState: <#T##UIControlState#>)
   }
 
 }
