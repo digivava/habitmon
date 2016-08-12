6312239ab4e38127261d90c04959704c9dcaafb7
Valerie <val.conklin@gmail.com>
2016-08-12 16:09:52 -0700

    Adds checkmark-related assets and ability to check off checkbox

--- Habitmon/Checkmark.swift
+++ Habitmon/Checkmark.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class Checkmark: UIView {
+  
+//  var habit: Habit!
 
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
     
     let checkbox = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-    checkbox.backgroundColor = UIColor.redColor()
+    //checkbox.backgroundColor = UIColor.redColor()
+    
+    let emptyCheckboxImage = UIImage(named: "emptyCheckbox")
+    let checkedOffImage = UIImage(named: "checkedOff")
+    
+    checkbox.setImage(emptyCheckboxImage, forState: .Normal)
+    checkbox.setImage(checkedOffImage, forState: .Selected)
+    
     
     checkbox.addTarget(self, action: #selector(Checkmark.checkboxTapped(_:)), forControlEvents: .TouchDown)
     
@@ -24,6 +33,11 @@ class Checkmark: UIView {
   
   func checkboxTapped(checkbox: UIButton) {
     print("Ka-ching!")
+    //checkbox.backgroundColor = UIColor.greenColor()
+    print(checkbox.selected)
+    checkbox.selected = true
+    print(checkbox.selected)
+//    checkbox.setImage(<#T##image: UIImage?##UIImage?#>, forState: <#T##UIControlState#>)
   }
 
 }
