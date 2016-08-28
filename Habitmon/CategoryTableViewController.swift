56dde8bce4c8a1d95f5e07287c5fda4855ae6adf
Valerie <val.conklin@gmail.com>
2016-08-28 16:33:35 -0700

    Pictures no longer squished on table view

--- Habitmon/CategoryTableViewController.swift
+++ Habitmon/CategoryTableViewController.swift
@@ -33,15 +33,25 @@ class CategoryTableViewController: UITableViewController {
     return habit
   }
   
-//  func seedHabits() {
-//    try! realm.write {
-//      realm.create(Habit.self, value: ["id": 1, "name": "No sweets", "category": "Diet", "evolution1": "Lollipup", "evolution2": "Sugpug", "evolution3": "Molassie", "profile1": "Happily feasts on all the sugar you aren't eating. Particularly enjoys smugly eating your favorites right next to you. But such a loyal and cuddly friend, you can't help but stay near it.", "profile2": "Hordes a stash of gooey, fresh-baked chocolate chip cookies in its doghouse and gets vicious if you try to snag any. Likes belly-rubs.", "profile3": "A powerful yet flighty spirit that rarely shows itself to humans. Legend says it comes only to those who prove themselves as truly self-disciplined."])
-//      
-//      realm.create(Habit.self, value: ["id": 2, "name": "Write 1000 words", "category": "Creative", "evolution1": "Wordvark", "evolution2": "Pengolin", "evolution3": "Bookupine", "profile1": "Patches of its skin open up to reveal inspiring passages from great works of literature, tragically relevant poetry, and occasionally trashy clickbait articles from the Internet.", "profile2": "Its ink-tipped tongue can produce manuscripts at 200 WPM. Does its best thinking in the early morning.", "profile3": "Its awkward introversion leads to it often being misunderstood. Uses its fountain-pen quills to work on many projects at once. Likes to be given coffee in its water bowl."])
-//      
-//    }
-//  }
-  
+  func seedHabits() {
+    try! realm.write {
+      realm.create(Habit.self, value: ["id": 1, "name": "No sweets", "category": "Diet", "evolution1": "Lollipup", "evolution2": "Sugpug", "evolution3": "Molassie", "profile1": "Happily feasts on all the sugar you aren't eating. Particularly enjoys smugly eating your favorites right next to you. But such a loyal and cuddly friend, you can't help but stay near it.", "profile2": "Hordes a stash of gooey, fresh-baked chocolate chip cookies in its doghouse and gets vicious if you try to snag any. Likes belly-rubs.", "profile3": "A powerful yet flighty spirit that rarely shows itself to humans. Legend says it comes only to those who prove themselves as truly self-disciplined."])
+      
+      realm.create(Habit.self, value: ["id": 2, "name": "Write 1000 words", "category": "Creative", "evolution1": "Wordvark", "evolution2": "Pengolin", "evolution3": "Bookupine", "profile1": "Patches of its skin open up to reveal inspiring passages from great works of literature, tragically relevant poetry, and occasionally trashy clickbait articles from the Internet.", "profile2": "Its ink-tipped tongue can produce manuscripts at 200 WPM. Does its best thinking in the early morning.", "profile3": "Its awkward introversion leads to it often being misunderstood. Uses its fountain-pen quills to work on many projects at once. Likes to be given coffee in its water bowl."])
+      
+      realm.create(Habit.self, value: ["id": 3, "name": "Practice foreign language", "category": "Study", "evolution1": "Speakter", "evolution2": "Bilinghoul", "evolution3": "Langwitch", "profile1": "A prideful Habitmon that is especially proficient in pig Latin. Its ghostly transparency frustrates you because of how squeezable it looks.", "profile2": "Knows all sorts of obscure, spooky nursery rhymes in other languages, and whispers them to you while you sleep.", "profile3": "It cooks a variety of foods from your target language's country, but if you don't like the taste it will screech profanities that you haven't learned yet."])
+      
+      realm.create(Habit.self, value: ["id": 4, "name": "Work out", "category": "Fitness", "evolution1": "Musscle", "evolution2": "Barnuckle", "evolution3": "Shellflex", "profile1": "Gives great motivational speeches. You are unsure of where it hides its unlimited supply of sports drinks.", "profile2": "Expert high-five giver and beloved by all, but often feels bashful about the fishy smell of its sweat.", "profile3": "Has high expectations, as a former trainer of Olympic athletes. Affectionately calls you 'pansy'."])
+      
+      realm.create(Habit.self, value: ["id": 5, "name": "Floss", "category": "Other", "evolution1": "Plaqodile", "evolution2": "Flossiraptor", "evolution3": "Pterodentyl", "profile1": "An expert at squeezing into tight spaces. Has complicated feelings toward mint ice cream.", "profile2": "A feisty Habitmon that is always trying to wedge itself into your mouth.", "profile3": "Very high-maintenance as it needs constant cleaning. But extremely loyal, and has powerful teeth-whitening properties."])
+      
+      realm.create(Habit.self, value: ["id": 6, "name": "Play musical instrument", "category": "Creative", "evolution1": "Songkong", "evolution2": "Orangutwang", "evolution3": "Chimphony", "profile1": "It can produce all sorts of music on its little triangle, including death metal and Mongolian throat-singing.", "profile2": "Loves to share music and invites you to a different concert every weekend. Prefers vinyl.", "profile3": "A world-renowned monkey composer. Always in a pretentious Internet flame war with his arch-nemesis A-Capellican over musical tastes."])
+      
+      realm.create(Habit.self, value: ["id": 7, "name": "Avoid carbs", "category": "Diet", "evolution1": "Butterhorn", "evolution2": "Buffaloaf", "evolution3": "Wildayeest", "profile1": "Jealously stampedes all over any grain product it notices you contemplating. Makes cute noises.", "profile2": "Smells delicious when warm. But if you try to nibble on it, it tastes like poison.", "profile3": "Fierce and strong, yet lean. Lets you ride it into the sunset sometimes."])
+    
+    }
+  }
+
   
   // MARK: - Actions
   
@@ -57,7 +67,7 @@ class CategoryTableViewController: UITableViewController {
     super.viewDidLoad()
     
     if realm.objects(Habit.self).count == 0 {
-      Habit().seedHabits()
+      seedHabits()
     }
     
     
--- Habitmon/Habit.swift
+++ Habitmon/Habit.swift
@@ -11,8 +11,6 @@ import RealmSwift
 
 class Habit: Object {
   
-  let realm = try! Realm()
-  
   // MARK: Properties
   
   let id = RealmOptional<Int>()
@@ -44,23 +42,6 @@ class Habit: Object {
   dynamic var profile1: String? = "A cute, lovable Habitmon."
   dynamic var profile2: String? = "A bratty teenage Habitmon."
   dynamic var profile3: String? = "A strong, majestic Habitmon."
-  
-  
-  func seedHabits() {
-    print("bleh")
-    try! realm.write {
-      realm.create(Habit.self, value: ["id": 1, "name": "No sweets", "category": "Diet", "evolution1": "Lollipup", "evolution2": "Sugpug", "evolution3": "Molassie", "profile1": "Happily feasts on all the sugar you aren't eating. Particularly enjoys smugly eating your favorites right next to you. But such a loyal and cuddly friend, you can't help but stay near it.", "profile2": "Hordes a stash of gooey, fresh-baked chocolate chip cookies in its doghouse and gets vicious if you try to snag any. Likes belly-rubs.", "profile3": "A powerful yet flighty spirit that rarely shows itself to humans. Legend says it comes only to those who prove themselves as truly self-disciplined."])
-      
-      realm.create(Habit.self, value: ["id": 2, "name": "Write 1000 words", "category": "Creative", "evolution1": "Wordvark", "evolution2": "Pengolin", "evolution3": "Bookupine", "profile1": "Patches of its skin open up to reveal inspiring passages from great works of literature, tragically relevant poetry, and occasionally trashy clickbait articles from the Internet.", "profile2": "Its ink-tipped tongue can produce manuscripts at 200 WPM. Does its best thinking in the early morning.", "profile3": "Its awkward introversion leads to it often being misunderstood. Uses its fountain-pen quills to work on many projects at once. Likes to be given coffee in its water bowl."])
-      
-      realm.create(Habit.self, value: ["id": 3, "name": "Practice foreign language", "category": "Study", "evolution1": "Speakter", "evolution2": "Bilinghoul", "evolution3": "Langwitch", "profile1": "A prideful Habitmon that is especially proficient in pig Latin. Its ghostly transparency frustrates you because of how squeezable it looks.", "profile2": "Knows all sorts of obscure, spooky nursery rhymes in other languages, and whispers them to you while you sleep.", "profile3": "It cooks a variety of foods from your target language's country, but if you don't like the taste it will screech profanities that you haven't learned yet."])
-      
-      realm.create(Habit.self, value: ["id": 4, "name": "Work out", "category": "Fitness", "evolution1": "Musscle", "evolution2": "Barnuckle", "evolution3": "Shellflex", "profile1": "Gives great motivational speeches. You are unsure of where it hides its unlimited supply of sports drinks.", "profile2": "Expert high-five giver and beloved by all, but often feels bashful about the fishy smell of its sweat.", "profile3": "Has high expectations, as a former trainer of Olympic athletes. Affectionately calls you 'pansy'."])
-      
-      
-      
-    }
-  }
   
   
 }
